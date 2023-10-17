084960b9f8f25facbc7e49a51fc73658f555eb4f
Egor <koropeczegor@gmail.com>
2023-10-17 19:38:45 +0300

    project creation

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addEventListener("mousemove", (e) => {
+  Object.assign(document.documentElement, {
+    style: `
+        --move-x: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
+        --move-y: ${(e.clientX - window.innerHeight / 2) * -0.01}deg;
+        `,
+  });
+});
