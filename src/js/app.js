5a43e7cc69dc6700aef37682082ce8ba9c4098d3
Egor <koropeczegor@gmail.com>
2023-10-22 16:09:22 +0300

    horizontal parallax effect

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,24 @@
 "use strict";
 
+//layers
 document.addEventListener("mousemove", (e) => {
   Object.assign(document.documentElement, {
     style: `
-        --move-x: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
-        --move-y: ${(e.clientX - window.innerHeight / 2) * -0.01}deg;
+        --move-x: ${(e.clientX - window.innerWidth / 2) * -0.003}deg;
+        --move-y: ${(e.clientX - window.innerHeight / 2) * -0.003}deg;
         `,
   });
+});
+
+//Slider
+new Swiper(".slider", {
+  // direction: "vertical",
+  speed: 2400,
+  mousewheel: {
+    enabled: true,
+    sensitivity: 2.4,
+  },
+  spaceBetween: 18,
+  parallax: true,
+  freeMode: true,
 });
