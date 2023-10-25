85e24eb0a6ec8ea42e6dfbe0efb277db3536b089
Egor <koropeczegor@gmail.com>
2023-10-25 23:45:55 +0300

    slider parallax

--- src/js/app.js
+++ src/js/app.js
@@ -11,8 +11,7 @@ document.addEventListener("mousemove", (e) => {
 });
 
 //Slider
-new Swiper(".slider", {
-  // direction: "vertical",
+const sliderWitcher = new Swiper(".slider__witcher", {
   speed: 2400,
   mousewheel: {
     enabled: true,
@@ -21,4 +20,45 @@ new Swiper(".slider", {
   spaceBetween: 18,
   parallax: true,
   freeMode: true,
+});
+
+// slider main
+
+const sliderMain = new Swiper(".slider__main", {
+  freeMode: true,
+  centeredSlides: true,
+  mousewheel: true,
+  parallax: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    680: {
+      slidesPerView: 3.5,
+      spaceBetween: 60,
+    },
+  },
+});
+
+const sliderBg = new Swiper(".slider__bg", {
+  centeredSlides: true,
+  parallax: true,
+  spaceBetween: 60,
+  slidesPerView: 3.5,
+});
+
+sliderMain.controller.control = sliderBg;
+
+document.querySelectorAll(".slider__main__item").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("opened");
+  });
+});
+
+let desc = document.querySelector(".description__main");
+sliderMain.on("slideChange", (e) => {
+  sliderMain.activeIndex > 0
+    ? desc.classList.add("hidden")
+    : desc.classList.remove("hidden");
 });
